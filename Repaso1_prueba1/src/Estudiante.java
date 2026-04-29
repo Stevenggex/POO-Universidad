959ef5fe003fe6cee90566c380e41513aa1ba905
Stevenggex <gallo.stevengc@gmail.com>
2026-04-28 20:05:01 -0500

    Tarea POO_60min_Herencia_Multinivel

--- Repaso1_prueba1/src/Estudiante.java
+++ Repaso1_prueba1/src/Estudiante.java
@@ -4,7 +4,6 @@ public class Estudiante {
     private double promedio;
     private double valorMatricula;
 
-    
     public Estudiante(String codigo, String nombre, double promedio, double valorMatricula) {
         this.codigo = codigo;
         this.nombre = nombre;
